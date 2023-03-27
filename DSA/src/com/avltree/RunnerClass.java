07a79a257aee3da17260e4638439d7e2c09c4f74
= <=>
2023-03-27 22:17:27 +0530

    Minor Changes were Made.

--- DSA/src/com/avltree/RunnerClass.java
+++ DSA/src/com/avltree/RunnerClass.java
@@ -18,18 +18,3 @@ public class RunnerClass
 		avlTree.levelOrder();
 	}
 }
-
-//AVLTree avlTree = new AVLTree();
-//avlTree.insert(10);
-//avlTree.insert(20);
-//avlTree.insert(30);
-//avlTree.insert(40);
-//avlTree.insert(50);
-//avlTree.insert(60);
-//avlTree.insert(70);
-//avlTree.insert(80);
-//avlTree.levelOrder(avlTree.rootNode);
-//avlTree.deleteNode(10);
-//System.out.println();
-//avlTree.levelOrder(avlTree.rootNode);
-//System.out.println(avlTree.rootNode.height);
