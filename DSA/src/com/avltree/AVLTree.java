2847dd482547a94045530c1429a1c708f62ea830
= <=>
2023-03-27 21:56:14 +0530

    Errors in Insert and Delete Methods are fixed.

--- DSA/src/com/avltree/AVLTree.java
+++ DSA/src/com/avltree/AVLTree.java
@@ -7,22 +7,17 @@ public class AVLTree 	// Adelson - Velsky and Landis
 {
 	BinaryNode rootNode;
 	
-	public AVLTree()
-	{
-		rootNode = null;
-	}
-	
-	// Height of the Node.
+	// Height of Node.
 	public int getHeight(BinaryNode rootNode)
 	{
 		if(rootNode == null)
 		{
-			return 0;
+			return -1;
 		}
 		return rootNode.height;
 	}
 	
-	// Difference between heights of LeftSubTree and RightSubTree.
+	// Balance.
 	public int getBalance(BinaryNode rootNode)
 	{
 		if(rootNode == null)
@@ -33,165 +28,167 @@ public class AVLTree 	// Adelson - Velsky and Landis
 	}
 	
 	// Right Rotation.
-	public BinaryNode rightRotation(BinaryNode disBalancedNode)
+	public BinaryNode rightRotation(BinaryNode rootNode)
 	{
-		BinaryNode newRoot = disBalancedNode.leftChild;
-		disBalancedNode.leftChild = newRoot.rightChild;
-		newRoot.rightChild = disBalancedNode;
-		disBalancedNode.height = 1 + Math.max(getHeight(disBalancedNode.leftChild), getHeight(disBalancedNode.rightChild));
+		BinaryNode newRoot = rootNode.leftChild;
+		rootNode.leftChild = newRoot.rightChild;
+		newRoot.rightChild = rootNode;
+		rootNode.height = 1 + Math.max(getHeight(rootNode.leftChild), getHeight(rootNode.rightChild));
 		newRoot.height = 1 + Math.max(getHeight(newRoot.leftChild), getHeight(newRoot.rightChild));
 		return newRoot;
 	}
 	
 	// Left Rotation.
-	public BinaryNode leftRotation(BinaryNode disBalancedNode)
+	public BinaryNode leftRotation(BinaryNode rootNode)
 	{
-		BinaryNode newRoot = disBalancedNode.rightChild;
-		disBalancedNode.rightChild = newRoot.leftChild;
-		newRoot.leftChild = disBalancedNode;
-		disBalancedNode.height = 1 + Math.max(getHeight(disBalancedNode.leftChild), getHeight(disBalancedNode.rightChild));
+		BinaryNode newRoot = rootNode.rightChild;
+		rootNode.rightChild = newRoot.leftChild;
+		newRoot.leftChild = rootNode;
+		rootNode.height = 1 + Math.max(getHeight(rootNode.leftChild), getHeight(rootNode.rightChild));
 		newRoot.height = 1 + Math.max(getHeight(newRoot.leftChild), getHeight(newRoot.rightChild));
 		return newRoot;
 	}
-		
-	// Insert Node
+	
+	// Insert Method Runner.
 	private BinaryNode insert(BinaryNode rootNode, int value)
 	{
 		if(rootNode == null)
 		{
 			BinaryNode newNode = new BinaryNode();
 			newNode.value = value;
+			newNode.height = 0;
 			return newNode;
 		}
-		else if(value < rootNode.value)
+		else if(value <= rootNode.value)
 		{
-			rootNode.leftChild =  insert(rootNode.leftChild, value);
+			rootNode.leftChild = insert(rootNode.leftChild, value);
 		}
-		else
+		else 
 		{
 			rootNode.rightChild = insert(rootNode.rightChild, value);
 		}
 		
 		rootNode.height = 1 + Math.max(getHeight(rootNode.leftChild), getHeight(rootNode.rightChild));
 		int balance = getBalance(rootNode);
-		
-		if(balance > 1 && value < rootNode.leftChild.value)	// LL Condition.
+		if(balance > 1 && value < rootNode.leftChild.value)
 		{
 			rootNode = rightRotation(rootNode);
+			return rootNode;
 		}
-		if(balance > 1 && value > rootNode.leftChild.value) // LR Condition.
+		if(balance > 1 && value > rootNode.leftChild.value)
 		{
 			rootNode.leftChild = leftRotation(rootNode.leftChild);
 			rootNode = rightRotation(rootNode);
+			return rootNode;
 		}
-		if(balance < -1 && value > rootNode.rightChild.value) // RR Condition.
+		if(balance < -1 && value > rootNode.rightChild.value)
 		{
 			rootNode = leftRotation(rootNode);
+			return rootNode;
 		}
-		if(balance < -1 && value < rootNode.rightChild.value) // RL Condition.
+		if(balance < -1 && value <= rootNode.rightChild.value) 
 		{
 			rootNode.rightChild = rightRotation(rootNode.rightChild);
 			rootNode = leftRotation(rootNode);
+			return rootNode;
 		}
 		return rootNode;
-		
-	} 
+	}
 	
+	// Insert Method.
 	public void insert(int value)
 	{
 		rootNode = insert(rootNode, value);
 	}
 	
-	// Minimum Node.
-	public BinaryNode minimiNode(BinaryNode node)
+	//Minimum Node.
+	public BinaryNode getMinimum(BinaryNode rootNode)
 	{
-		if(node.leftChild == null)
+		if(rootNode.leftChild == null)
 		{
-			return node;
+			return rootNode;
 		}
-		return minimiNode(node.leftChild);
+		return getMinimum(rootNode.leftChild); 
 	}
 	
-	// Delete Node.
-	public BinaryNode deleteNode(BinaryNode rootNode, int value)
+	// Delete Method Runner.
+	private BinaryNode delete(BinaryNode rootNode, int value)
 	{
 		if(rootNode == null)
 		{
-			System.out.println("Tree is Empty");
+			System.out.println("Element is not in the AVL Tree");
 			return null;
 		}
 		else if(value < rootNode.value)
 		{
-			rootNode.leftChild = deleteNode(rootNode.leftChild, value);
+			rootNode.leftChild = delete(rootNode.leftChild, value);
 		}
 		else if(value > rootNode.value)
 		{
-			rootNode.rightChild = deleteNode(rootNode.rightChild, value);
+			rootNode.rightChild = delete(rootNode.rightChild, value);
 		}
 		else 
 		{
-			if(rootNode.leftChild != null && rootNode.rightChild!= null)	// Node has two children.
+			if(rootNode.leftChild != null && rootNode.rightChild != null)
 			{
-				BinaryNode minNodeforRight = minimiNode(rootNode.rightChild);
-				rootNode.value = minNodeforRight.value;
-				rootNode.rightChild = deleteNode(rootNode.rightChild, minNodeforRight.value);
+				BinaryNode minNodeForRight = getMinimum(rootNode.rightChild);
+				rootNode.value = minNodeForRight.value;
+				rootNode.rightChild = delete(rootNode.rightChild, minNodeForRight.value);
 			}
-			else if (rootNode.leftChild != null) 		// Node has one left children.
-			{	
+			else if(rootNode.leftChild != null)
+			{
 				rootNode = rootNode.leftChild;
 			}
-			else if (rootNode.rightChild != null) 		// Node has one right children.
+			else if(rootNode.rightChild != null)
 			{
 				rootNode = rootNode.rightChild;
 			}
-			else										// Node to be deleted is a leaf Node. 
+			else 
 			{
 				rootNode = null;
 			}
-		}
-		
+		}	
 		int balance = getBalance(rootNode);
-		
-		if(balance > 1 && getBalance(rootNode.leftChild) > 0)	// LL Condition.
+		if(balance > 1 && value < rootNode.leftChild.value)
 		{
 			rootNode = rightRotation(rootNode);
+			return rootNode;
 		}
-		if(balance > 1 && getBalance(rootNode.leftChild) < 0)	// LR Condition.
+		if(balance > 1 && value > rootNode.leftChild.value)
 		{
 			rootNode.leftChild = leftRotation(rootNode.leftChild);
 			rootNode = rightRotation(rootNode);
+			return rootNode;
 		}
-		if(balance < -1 && getBalance(rootNode.rightChild) < 0)	// RR Condition.
+		if(balance < -1 && value > rootNode.rightChild.value)
 		{
 			rootNode = leftRotation(rootNode);
+			return rootNode;
 		}
-		if(balance < -1 && getBalance(rootNode.rightChild) > 0)	// LR Condition.
+		if(balance < -1 && value <= rootNode.rightChild.value) 
 		{
 			rootNode.rightChild = rightRotation(rootNode.rightChild);
-			rootNode = leftRotation(rootNode.rightChild);
+			rootNode = leftRotation(rootNode);
+			return rootNode;
 		}
 		return rootNode;
 	}
 	
-	public void deleteNode(int value)
+	// Delete Method.
+	public void delete(int value)
 	{
-		rootNode = deleteNode(rootNode, value);
+		rootNode = delete(rootNode, value);
 	}
 	
-	// Level Order Traversal.
-	public void levelOrder(BinaryNode rootNode)
+	// Level Order.
+	public void levelOrder()
 	{
-		if(rootNode == null)
-		{
-			System.out.println("Tree is Empty !!!");
-			return;
-		}
 		Queue<BinaryNode> queue = new LinkedList<>();
 		queue.add(rootNode);
 		while(!queue.isEmpty())
 		{
 			BinaryNode presentNode = queue.remove();
-			System.out.print(presentNode.value+" ");
+			System.out.print(presentNode.value+" - "+presentNode.height+" ");
 			if(presentNode.leftChild != null)
 			{
 				queue.add(presentNode.leftChild);
--- DSA/src/com/avltree/RunnerClass.java
+++ DSA/src/com/avltree/RunnerClass.java
@@ -2,20 +2,34 @@ package com.avltree;
 
 public class RunnerClass 
 {
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		AVLTree avlTree = new AVLTree();
 		avlTree.insert(10);
 		avlTree.insert(20);
 		avlTree.insert(30);
+		avlTree.insert(30);
 		avlTree.insert(40);
 		avlTree.insert(50);
 		avlTree.insert(60);
-		avlTree.insert(70);
-		avlTree.insert(80);
-		avlTree.levelOrder(avlTree.rootNode);
-		avlTree.deleteNode(10);
+		avlTree.levelOrder();
 		System.out.println();
-		avlTree.levelOrder(avlTree.rootNode);
-		System.out.println(avlTree.rootNode.height);
+		avlTree.delete(30);
+		avlTree.levelOrder();
 	}
 }
+
+//AVLTree avlTree = new AVLTree();
+//avlTree.insert(10);
+//avlTree.insert(20);
+//avlTree.insert(30);
+//avlTree.insert(40);
+//avlTree.insert(50);
+//avlTree.insert(60);
+//avlTree.insert(70);
+//avlTree.insert(80);
+//avlTree.levelOrder(avlTree.rootNode);
+//avlTree.deleteNode(10);
+//System.out.println();
+//avlTree.levelOrder(avlTree.rootNode);
+//System.out.println(avlTree.rootNode.height);
